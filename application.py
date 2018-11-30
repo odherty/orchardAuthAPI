53ca17e51dffc331730b7492b30d0f338d93eee7
Patrick O <patrick.odherty@gmail.com>
2018-11-30 16:05:27 -0500

    updated file names for azure deployment

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'apiServ.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
