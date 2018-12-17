18e4e44c21ab841117c8af4d1d4720113f8b77ba
Patrick O <patrick.odherty@gmail.com>
2018-12-17 15:53:19 -0500

    Changed DB from SQLite to MongoDB for more thread-friendlyness

--- app.py
+++ app.py
@@ -1,76 +1,75 @@
-from flask import Flask, request, jsonify, render_template, redirect, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.pool import QueuePool
-from flask_marshmallow import Marshmallow
 import os
+from flask import Flask, request, jsonify, render_template, redirect, Response
+from flask_pymongo import PyMongo
+import pymongo
+from pymongo import MongoClient
+from bson import json_util
+from bson.json_util import dumps, CANONICAL_JSON_OPTIONS
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
+client = MongoClient('localhost', 27017)
+mongodb = client.authDB
 
-class Client(db.Model):
-    clientID = db.Column(db.Integer, primary_key=True, unique=True)
-    redirect = db.Column(db.String(150))
-
-    def __init__(self, clientID, redirect):
-        self.clientID = clientID
-        self.redirect = redirect
-
-
-class ClientSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ('clientID','redirect')
-
-
-client_schema = ClientSchema()
-clients_schema = ClientSchema(many=True)
-
-
-@app.route("/login/")
+@app.route("/login")
 def index():
 
     #add client if not already added
-    clientID = db.session.query(Client.clientID).filter_by(clientID=request.args.get("client_id")).scalar()
-    if (clientID is None) :
-        params = request.args
-        new_client = Client(request.args.get('client_id'), request.args.get('redirect_uri'))
-        db.session.add(new_client)
-        db.session.flush()
-        db.session.commit()
+    # clientID = db.session.query(Client.clientID).filter_by(clientID=request.args.get("client_id")).scalar()
+    params = request.args
+    clientID = request.args.get('clientID')
+    redirectURI = request.args.get('redirectURI')
+
+    if (clientID is None or redirectURI is None) :
+        return "Invalid Arguments"
+
+    client = mongodb.users.find_one({"clientID" : clientID})
+    if (client is None) :
+        mongodb.users.insert_one({"clientID": clientID, "redirect": redirectURI })
         return render_template("new_user.html", params = params)
     else: 
-        # print("Didn't add")
         return render_template("existing_user.html")
-    db.session.close()
 
 @app.route("/read", methods=["GET"])
 def read():
-    all_clients = Client.query.all()
-    result = clients_schema.dump(all_clients)
-    return jsonify(result.data)
+    #We define a python list 'result' to collect results from mongoDB
+    #Note: a python 'list' is defined by [] while a python 'dictonary' is defined by {}, these are two different things
+    result = []
+    #The mongoDB find command returns a 'cursor' which can be iterated through to get data
+    data = mongodb.users.find({})
+    #Iterate through cursor
+    for doc in data:
+        result.append(doc)
+    # print(result) for debug
+    #json_util works with BSON objects, BSON is the JSON-like format that mongoDB uses
+    #the mimetype parameter lets it output it in a nice, human readable format to the Flask page
+    return Response(json_util.dumps(result), mimetype='application/json')
 
 # endpoint to get client detail by id
 @app.route("/read/<clientID>", methods=["GET"])
 def clientdetail(clientID):
-    client = Client.query.get(clientID)
-    #redirect_delete(clientID)     
-    return client_schema.jsonify(client)
+    print(clientID)
+    client = mongodb.users.find_one({"clientID": clientID})
+    print(client)
+    return Response(json_util.dumps(client), mimetype='application/json')
 
 # endpoint to get client detail by id
 @app.route("/complete/<clientID>", methods=["GET"])
 def complete_redirect(clientID):
     #Grab authorization code from FB (in url string)
     code = str(request.args.get("code"))
+    print(code)
+    if (code == "None"):
+        return "Invalid Arguments"
+
     #Grab redirect uri
-    redirectURI = Client.query.filter_by(clientID=clientID).first().redirect
+    redirectURI = mongodb.users.find_one({"clientID" : clientID})['redirect']
     redirectURI += "?code=" + code
 
     #Go to redirectURI with code
-    return redirect(redirectURI)
+    return redirectURI
+    # return redirect(redirectURI)
 
 if __name__ == '__main__':
     app.run(debug=True)
