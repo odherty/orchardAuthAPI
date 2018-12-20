1e257228e393d01738e776dfc8fbff891a1cd87f
Patrick O <patrick.odherty@gmail.com>
2018-12-20 10:58:18 -0500

    fixing string error in delete function, reverting previous commit

--- app.py
+++ app.py
@@ -72,10 +72,10 @@ def clientDelete(clientEMail):
 @app.route("/complete", methods=["GET"])
 def complete_redirect():
     #Grab authorization code from FB (in url string)
-    clientEMail = request.args.get("client_email")
+    clientEMail = str(request.args.get("client_email"))
     code = str(request.args.get("code"))
     print(code)
-    if (code == "None" or clientEMail is None):
+    if (code == "None" or clientEMail == "None"):
         return "Invalid Arguments"
 
     #Grab redirect uri
