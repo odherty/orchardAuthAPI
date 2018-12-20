e4d7a2b2b87ccdabb658a62dae8f4dda5c14a22b
Patrick O <patrick.odherty@gmail.com>
2018-12-20 11:20:11 -0500

    fixed syntax, system revolves around the state sent

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ def index():
     # clientID = db.session.query(Client.clientID).filter_by(clientID=request.args.get("client_id")).scalar()
     params = request.args
     clientID = request.args.get('client_id')
-    clientEMail = request.args.get('client_email')
+    state = request.args.get('state')
     redirectURI = request.args.get('redirect_uri')
 
-    if (clientID is None or redirectURI is None or clientEMail is None) :
+    if (clientID is None or redirectURI is None or state is None) :
         return "Invalid Arguments"
 
-    client = mongodb.users.find_one({"clientEMail" : clientEMail})
+    client = mongodb.users.find_one({"state" : state})
     if (client is None) :
-        mongodb.users.insert_one({"clientEMail": clientEMail, "redirect": redirectURI })
+        mongodb.users.insert_one({"state": state, "redirect": redirectURI })
         return render_template("new_user.html", params = params)
     else: 
         return render_template("existing_user.html", params= params)
@@ -51,28 +51,26 @@ def read():
     return Response(json_util.dumps(result), mimetype='application/json')
 
 # endpoint to get client detail by id
-@app.route("/read/<clientEMail>", methods=["GET"])
-def clientDetail(clientEMail):
+@app.route("/read/<state>", methods=["GET"])
+def clientDetail(state):
     client = mongodb.users.find_one({"clientEMail": clientEMail})
     return Response(json_util.dumps(client), mimetype='application/json')
 
-@app.route("/delete/<clientEMail>", methods=["GET"])
-def clientDelete(clientEMail):
+@app.route("/delete/<state>", methods=["GET"])
+def clientDelete(state):
     clientID = request.args.get("client_id")
-    clientEMail = request.args.get("client_email")
     redirectURI = request.args.get("redirect_uri")
-    state = request.args.get("state")
-    if (clientID is None or redirectURI is None or clientEMail is None or state is None) :
+    if (clientID is None or redirectURI is None) :
         return "Invalid Arguments"
 
-    mongodb.users.delete_one({"clientEMail": clientEMail})
-    return redirect("login?client_id=" + clientID + "&client_email=" + clientEMail + "&redirect_uri=" + redirectURI) + "&state=" + state
+    mongodb.users.delete_one({"state": state})
+    return redirect("login?client_id=" + clientID + "&redirect_uri=" + redirectURI + "&state=" + state)
 
 # endpoint to get client detail by id
 @app.route("/complete", methods=["GET"])
 def complete_redirect():
     #Grab authorization code from FB (in url string)
-    clientEMail = str(request.args.get("client_email"))
+    state = str(request.args.get("state"))
     code = str(request.args.get("code"))
     print(code)
     if (code == "None" or clientEMail == "None"):
